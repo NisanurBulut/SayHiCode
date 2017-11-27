c11825ac73fd55b6ff68dd51aff023cca16417d3
NisanurBulut <nisanurrunasin@gmail.com>
2017-11-27 07:10:59 +0300

    tüm anonim kullanıcılar için, Kimliği doğrulanmış olsun veya olmasın, her tür talebin erişebileceği methodu tanımladım.

--- webApiTokenAuthentication/Controllers/DataController.cs
+++ webApiTokenAuthentication/Controllers/DataController.cs
@@ -9,6 +9,15 @@ namespace webApiTokenAuthentication.Controllers
 {
     public class DataController : ApiController
     {
-    
+       // Bu eylemi tüm anonim kullanıcılar için ekledim.
+       //Kimliği doğrulanmış olsun veya olmasın, her tür talep bu işleme erişebilir.
+
+       [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Şu an Server'da zaman: " + DateTime.Now.ToString());
+        }
     }
 }
