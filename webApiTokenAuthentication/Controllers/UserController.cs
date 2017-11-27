d96aadcb09a0e97dc435f1bc5d61967ff39c65fc
NisanurBulut <nisanurrunasin@gmail.com>
2017-11-27 11:44:38 +0300

    Kullanıcı kayıt olma işlemi için prototype fonksıyon olusturuldu

---
+++ webApiTokenAuthentication/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using webApiTokenAuthentication.Models;
+
+namespace webApiTokenAuthentication.Controllers
+{
+    public class UserController : ApiController
+    {
+        //[Authorize]
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/user/signup")]
+        public IHttpActionResult Signup(User userData)
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Merhaba " + identity.Name);
+        }
+    }
+}
---
+++ webApiTokenAuthentication/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApiTokenAuthentication.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+       
+    }
+}
