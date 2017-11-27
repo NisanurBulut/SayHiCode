7dda4e60103df6e95b48946b69c67f0248a0e1d5
NisanurBulut <nisanurrunasin@gmail.com>
2017-11-27 07:12:08 +0300

    Yönetici kullanıcısı veya normal kullanıcı olsun, kimliği doğrulanmış tüm kullanıcı türleri için eylem ekledim.

--- webApiTokenAuthentication/Controllers/DataController.cs
+++ webApiTokenAuthentication/Controllers/DataController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace webApiTokenAuthentication.Controllers
@@ -18,6 +19,17 @@ namespace webApiTokenAuthentication.Controllers
         public IHttpActionResult Get()
         {
             return Ok("Şu an Server'da zaman: " + DateTime.Now.ToString());
+        }
+
+
+        //Yönetici kullanıcısı veya normal kullanıcı olsun, kimliği doğrulanmış tüm kullanıcı türleri için bu eylemi ekledim.
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/authenticate")]
+        public IHttpActionResult GetForAuthenticate()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Hello " + identity.Name);
         }
     }
 }
