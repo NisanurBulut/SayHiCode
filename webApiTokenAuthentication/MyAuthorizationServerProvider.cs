8a95a9db5d4c18d79a2d83639454fadfb911aac0
NisanurBulut <nisanurrunasin@gmail.com>
2017-11-27 06:54:45 +0300

    admin user tanımlaması yapıldı

--- webApiTokenAuthentication/MyAuthorizationServerProvider.cs
+++ webApiTokenAuthentication/MyAuthorizationServerProvider.cs
@@ -1,11 +1,42 @@
-﻿using System;
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace webApiTokenAuthentication
 {
-    public class MyAuthorizationServerProvider
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
     {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); // 
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Nisanur Bulut"));
+                context.Validated(identity);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Uğur Gürel"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Kullanıcı adı ya da parola hatalıdır.");
+                return;
+            }
+        }
     }
 }
