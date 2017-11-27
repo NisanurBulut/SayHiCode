d6e30a5378383097dc42d1c5b52bd88c7a5115d9
NisanurBulut <nisanurrunasin@gmail.com>
2017-11-27 07:04:07 +0300

    appStart class yazıldı

---
+++ webApiTokenAuthentication/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(webApiTokenAuthentication.Startup))]
+
+namespace webApiTokenAuthentication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var myProvider = new MyAuthorizationServerProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = myProvider
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
