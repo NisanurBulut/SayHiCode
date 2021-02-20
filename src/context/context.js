e14b0a8ba5087c1b71b49f0e49ffb67a24b4ccb1
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 19:58:32 +0300

    implemented check request limit

--- src/context/context.js
+++ src/context/context.js
@@ -14,7 +14,27 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [githubRepos, setGithubRepos] = useState(mockRepos);
   const [githubFollowers, setGithubFollower] = useState(mockFollowers);
-
+  const [requests, setRequests] = useState(0);
+  const [loading, stIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+  //  check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, 'sorry, you have exceeded your hourly rate limit!');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+  useEffect(() => {}, []);
   return (
     <GithubContext.Provider
       value={{ githubUser, githubRepos, githubFollowers }}
