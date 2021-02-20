151a838b153574f4ca43bde9a8c8c02697be6546
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 17:11:10 +0300

    implemented GithubContext

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,12 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+// Provider, consumer-GithubContext
+
+const GithubProvider = ({ children }) => {
+  return <GithubContext.Provider value={'Say Hi Code'}>{children}</GithubContext.Provider>;
+};
+export {GithubProvider,GithubContext};
