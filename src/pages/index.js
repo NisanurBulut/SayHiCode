95ce956dc58f81a31a169081af9f1cfd29737f66
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 19:47:38 +0300

    deleted authWrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Dashboard from "./Dashboard";
-import AuthWrapper from "./AuthWrapper";
 import PrivateRoute from "./PrivateRoute";
 import Error from "./Error";
 
-export { Dashboard, AuthWrapper, PrivateRoute, Error };
+export { Dashboard, PrivateRoute, Error };
