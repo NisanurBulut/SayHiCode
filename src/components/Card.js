8355e0b5ed7c02e7a6c4cc51a72d48fd8eb4c076
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 18:36:33 +0300

    implemented user card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,36 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser;
+  console.log(githubUser);
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'NisanurBulut'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className="bio">{bio}</p>
+      <div className="links">
+        <p><MdBusiness></MdBusiness>{company}</p>
+        <p><MdLocationOn></MdLocationOn>{location || 'earth'}</p>
+        <a href={`https://${blog}`}><MdLink></MdLink>{blog}</a>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
