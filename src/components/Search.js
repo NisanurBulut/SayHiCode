0bfabaa344d58157329cb80b3ff50bc292f67daf
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 19:53:22 +0300

    implemented search components

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,32 @@ import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState('');
+  // get things from global context
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+    }
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="search a github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+        <h3>Requests : 60 / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
