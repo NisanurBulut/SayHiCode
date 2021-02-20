264be0d51e350e50f6a140ab7513664cb6192af6
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 19:44:02 +0300

    implemented column chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Dashboard from "./Dashboard";
-import Login from "./Login";
 import AuthWrapper from "./AuthWrapper";
 import PrivateRoute from "./PrivateRoute";
 import Error from "./Error";
 
-export { Dashboard, Login, AuthWrapper, PrivateRoute, Error };
+export { Dashboard, AuthWrapper, PrivateRoute, Error };
