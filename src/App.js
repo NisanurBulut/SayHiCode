7da26054250cd3eb08d3b398e879086095c8224c
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 19:42:41 +0300

    delete login component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { Dashboard, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -8,9 +8,6 @@ function App() {
       <Switch>
       <Route path="/" exact={true}>
       <Dashboard></Dashboard>
-      </Route>
-      <Route path="/login">
-      <Login />
       </Route>
       <Route path="*">
       <Error />
