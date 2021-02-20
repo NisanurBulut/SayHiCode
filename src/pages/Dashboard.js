6b55feb90730614e8169c29629c92ed9da8f6ab6
NisanurBulut <nbulut@sarplojistik.com>
2021-02-20 20:15:56 +0300

    implemented loading image for http request

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,13 +3,21 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
+  const { isLoading } = React.useContext(GithubContext);
+  if (isLoading) {
+    return (
+      <main>
+        <Search />
+        <img src={loadingImage} className='loading-img' alt='loding' />
+      </main>
+    );
+  }
   return (
     <main>
-      <Navbar></Navbar>
-      <Search/>
-      <Info/>
-      <User/>
-      <Repos/>
+      <Search />
+      <Info />
+      <User />
+      <Repos />
     </main>
   );
 };
