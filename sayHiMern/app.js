e7dae44d7331c9dc7c534ca16569083fe6bc26c3
NisanurBulut <nbulut@sarplojistik.com>
2021-02-27 10:04:58 +0300

    graphql api run

--- sayHiMern/app.js
+++ sayHiMern/app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-const app= express();
+const app = express();
 
 app.use(bodyParser.json());
-app.get("/",(req,res,next)=>{
-    res.send('Hello World');
-})
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events:[String!]!
+    }
+    type RootMutation{
+        CreateEvent(name:String):String
+    }
+    schema{
+        query:RootQuery
+        mutation:RootMutation
+    }
+    `),
+    rootValue: {
+      events: (args) => {
+        return ['Nisanur', 'Furkan'];
+      },
+      CreateEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql:true
+  })
+);
+
+
 app.listen(3000);
