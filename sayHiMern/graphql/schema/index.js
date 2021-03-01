67dabd13bf6189abeccef01dc71b3e566f804f71
NisanurBulut <nbulut@sarplojistik.com>
2021-03-01 14:01:39 +0300

    updated schema with login event

--- sayHiMern/graphql/schema/index.js
+++ sayHiMern/graphql/schema/index.js
@@ -22,6 +22,11 @@ type User {
   password: String
   createdEvents: [Event!]
 }
+type AuthData{
+  userId:ID!
+  token:String!
+  tokenExpiration:Int!
+}
 input EventInput {
   title: String!
   description: String!
@@ -35,6 +40,7 @@ input UserInput {
 type RootQuery {
     events: [Event!]!
     bookings:[Booking!]!
+    login(email:String!,password:String!):AuthData!
 }
 type RootMutation {
     createEvent(eventInput: EventInput): Event
