365bda6cc5bc8e95b6b4eb010dafae9fa4c5529c
NisanurBulut <nbulut@sarplojistik.com>
2021-03-01 12:17:38 +0300

    DRY Principle is implemented

--- sayHiMern/graphql/resolvers/index.js
+++ sayHiMern/graphql/resolvers/index.js
@@ -3,16 +3,19 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
 
+const transformEvent = (event) =>{
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: new Date(event._doc.date).toISOString(),
+    creator: user.bind(this, event.creator),
+  };
+}
 const events = async (eventIds) => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
     return events.map((event) => {
-      return {
-        ...event._doc,
-        _id: event.id,
-        date: new Date(event._doc.date).toISOString(),
-        creator: user.bind(this, event.creator),
-      };
+      return transformEvent(event);
     });
   } catch (err) {
     throw err;
@@ -21,11 +24,7 @@ const events = async (eventIds) => {
 const singleEvent = async (eventId) => {
   try {
     const event = await Event.findById(eventId);
-    return {
-      ...event._doc,
-      _id: event.id,
-      creator: user.bind(this, event.creator),
-    };
+    return transformEvent(event);
   } catch (err) {
     throw err;
   }
@@ -48,12 +47,7 @@ module.exports = {
     try {
       const events = await Event.find();
       return events.map((event) => {
-        return {
-          ...event._doc,
-          _id: event.id,
-          date: new Date(event._doc.date).toISOString(),
-          creator: user.bind(this, event._doc.creator),
-        };
+        return transformEvent(event);
       });
     } catch (err) {
       throw err;
@@ -87,12 +81,7 @@ module.exports = {
     let createdEvent;
     try {
       const result = await event.save();
-      createdEvent = {
-        ...result._doc,
-        _id: result._doc._id.toString(),
-        date: new Date(event._doc.date).toISOString(),
-        creator: user.bind(this, result._doc.creator),
-      };
+      createdEvent = transformEvent(result);
       const creator = await User.findById('603b9c3e2031073b78512b10');
 
       if (!creator) {
@@ -148,11 +137,7 @@ module.exports = {
   cancelBooking: async (args) => {
     try {
       const booking = await Booking.findById(args.bookingId).populate('event');
-      const event = {
-        ...booking.event._doc,
-        _id: booking.event.id,
-        creator: user.bind(this, booking._doc.creator),
-      };
+      const event = transformEvent(booking.event);
       await Booking.deleteOne({ _id: args.bookingId });
       return event;
     } catch (err) {
