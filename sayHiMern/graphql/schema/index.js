c1bbc111f53626e0164e3977c4e12fd450028ea4
NisanurBulut <nbulut@sarplojistik.com>
2021-03-01 10:42:01 +0300

    implemented Booking Schema

--- sayHiMern/graphql/schema/index.js
+++ sayHiMern/graphql/schema/index.js
@@ -1,5 +1,13 @@
 const { buildSchema } = require('graphql');
 module.exports = buildSchema(`
+
+type Booking {
+  _id: ID!
+  event:Event!
+  user:User!
+  createdAt:String!
+  updatedAt:String!
+}
 type Event {
   _id: ID!
   title: String!
@@ -26,10 +34,13 @@ input UserInput {
 }
 type RootQuery {
     events: [Event!]!
+    bookings:[Booking!]!
 }
 type RootMutation {
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
+    bookEvent(eventId:ID!):Booking
+    cancelBooking(bookingId:ID!):Event!
 }
 schema {
     query: RootQuery
