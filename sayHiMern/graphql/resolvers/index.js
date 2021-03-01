ec7156f4a0eaff87366ecddb805641606f3c4318
NisanurBulut <nbulut@sarplojistik.com>
2021-03-01 13:11:38 +0300

    implemented DRY Principle for booking

--- sayHiMern/graphql/resolvers/index.js
+++ sayHiMern/graphql/resolvers/index.js
@@ -2,16 +2,26 @@ const bcrypt = require('bcryptjs');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
-const {dateToString} = require('../../helpers/dateHelper');
+const { dateToString } = require('../../helpers/dateHelper');
 
-const transformEvent = (event) =>{
+const transformEvent = (event) => {
   return {
     ...event._doc,
     _id: event.id,
     date: dateToString(event._doc.date),
     creator: user.bind(this, event.creator),
   };
-}
+};
+const transformBooking = (booking) => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt),
+  };
+};
 const events = async (eventIds) => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
@@ -57,15 +67,8 @@ module.exports = {
   bookings: async () => {
     try {
       const bookings = await Booking.find();
-      return bookings.map(booking => {
-        return {
-          ...booking._doc,
-          _id: booking.id,
-          user: user.bind(this, booking._doc.user),
-          event: singleEvent.bind(this, booking._doc.event),
-          createdAt: dateToString(booking._doc.createdAt),
-          updatedAt: dateToString(booking._doc.updatedAt)
-        };
+      return bookings.map((booking) => {
+        return transformBooking(booking);
       });
     } catch (err) {
       throw err;
@@ -126,14 +129,7 @@ module.exports = {
       event: fetchedEvent,
     });
     const result = await booking.save();
-    return {
-      ...result._doc,
-      _id: result.id,
-      user: user.bind(this, booking._doc.user),
-      event: singleEvent.bind(this, booking._doc.event),
-      createdAt: dateToString(result._doc.createdAt),
-      updatedAt: dateToString(result._doc.updatedAt),
-    };
+    return transformBooking(result);
   },
   cancelBooking: async (args) => {
     try {
