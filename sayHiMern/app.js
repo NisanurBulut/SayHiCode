8594a4cafad25a696025e2a64a4d34265d432450
NisanurBulut <nbulut@sarplojistik.com>
2021-03-02 21:03:55 +0300

    used middleware

--- sayHiMern/app.js
+++ sayHiMern/app.js
@@ -4,9 +4,10 @@ const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const grapqlSchema=require('./graphql/schema/index');
 const graphqlResolvers=require('./graphql/resolvers/index');
-
+const isAuth = require('./middleware/is-auth');
 const app = express();
 
+app.use(isAuth);
 app.use(bodyParser.json());
 
 app.use(
