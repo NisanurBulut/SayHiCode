b38a6c5b027457fae21929480c0cbf9fe6589829
NisanurBulut <nbulut@sarplojistik.com>
2021-03-03 21:08:47 +0300

    delete empty code lines

--- sayHiMern/frontend/src/pages/AuthPage.js
+++ sayHiMern/frontend/src/pages/AuthPage.js
@@ -38,7 +38,6 @@ export class AuthPage extends Component {
           `
         };
 
-
       fetch('http://localhost:8000/graphql', {
         method: 'POST',
         body: JSON.stringify(requestBody),
