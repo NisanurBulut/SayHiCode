8ccf61fd47aa2e2d63e7c8cefe01097a9f1e0ce0
NisanurBulut <nbulut@sarplojistik.com>
2021-03-03 18:46:27 +0300

    created pages and implemented routing

--- sayHiMern/frontend/src/App.js
+++ sayHiMern/frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Merhaba Nisanur :) !</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={null} />
+        <Route path="/bookings" component={null} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ sayHiMern/frontend/src/pages/AuthPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AuthPage extends Component {
+    render() {
+        return (
+            <div>
+                Auth Page
+            </div>
+        )
+    }
+}
+
+export default AuthPage
