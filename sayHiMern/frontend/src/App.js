881c38183b46f230ee657897ecc5f8c3d324b048
NisanurBulut <nbulut@sarplojistik.com>
2021-03-03 22:05:58 +0300

    accordingto isLogin redirect pages

--- sayHiMern/frontend/src/App.js
+++ sayHiMern/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
@@ -8,24 +8,57 @@ import MainNavigation from './components/Navigation/MainNavigation';
 import './App.css';
 import AuthContext from './context/auth-context';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <React.Fragment>
-        <AuthContext.Provider value={{ token: null, userId: null }}>
-          <MainNavigation />
-          <main className="mainContent">
-            <Switch>
-              <Redirect from="/" to="/auth" exact />
-              <Route path="/auth" component={AuthPage} />
-              <Route path="/events" component={EventsPage} />
-              <Route path="/bookings" component={BookingsPage} />
-            </Switch>
-          </main>
-        </AuthContext.Provider>
-      </React.Fragment>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null,
+  };
+  login = (token, userId, tokenExpiration) => {
+    this.setState({
+      token: token,
+      userId: userId,
+    });
+  };
+  logout = () => {
+    this.setState({
+      token: null,
+      userId: null,
+    });
+  };
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <AuthContext.Provider
+            value={{
+              token: this.state.token,
+              userId: this.state.userId,
+              login: this.login,
+              logout: this.logout,
+            }}
+          >
+            <MainNavigation />
+            <main className="mainContent">
+              <Switch>
+                {!this.state.token && <Redirect from="/" to="/auth" exact />}
+                {this.state.token && <Redirect from="/" to="/events" exact />}
+                {this.state.token && <Redirect from="/auth" to="/events" exact />}
+                {!this.state.token && (
+                  <Route path="/auth" component={AuthPage} />
+                )}
+                {this.state.token && (
+                  <Route path="/events" component={EventsPage} />
+                )}
+                {this.state.token && (
+                  <Route path="/bookings" component={BookingsPage} />
+                )}
+              </Switch>
+            </main>
+          </AuthContext.Provider>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- sayHiMern/frontend/src/components/Navigation/MainNavigation.js
+++ sayHiMern/frontend/src/components/Navigation/MainNavigation.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './MainNavigation.css';
+import AuthContext from '../../context/auth-context';
 
 const MainNavigation = (props) => {
   return (
-    <header className="mainNavigation">
-      <div className="mainNavigationLogo">
-        <h1>Easy Event</h1>
-      </div>
-      <nav className="mainNavigationItems">
-          <ul>
-          <li>
-            <NavLink to="/auth">Authenticate</NavLink>
-            </li>
-            <li>
-            <NavLink to="/events">Events</NavLink>
-            </li>
-            <li>
-            <NavLink to="/bookings">Bookings</NavLink>
-            </li>
-          </ul>
-      </nav>
-    </header>
+    <AuthContext.Consumer>
+      {(context) => {
+        return (
+          <header className="mainNavigation">
+            <div className="mainNavigationLogo">
+              <h1>Easy Event</h1>
+            </div>
+            <nav className="mainNavigationItems">
+              <ul>
+                {!context.token && (
+                  <li>
+                    <NavLink to="/auth">Authenticate</NavLink>
+                  </li>
+                )}
+                {context.token && (
+                  <li>
+                    <NavLink to="/events">Events</NavLink>
+                  </li>
+                )}
+                {context.token && (
+                  <li>
+                    <NavLink to="/bookings">Bookings</NavLink>
+                  </li>
+                )}
+              </ul>
+            </nav>
+          </header>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 };
 
