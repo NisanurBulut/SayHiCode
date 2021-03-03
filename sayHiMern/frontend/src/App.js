9d770c53bde1e8cf10cafbe56e517a6b5cc6bb92
NisanurBulut <nbulut@sarplojistik.com>
2021-03-03 18:48:18 +0300

    inserted all page into routing

--- sayHiMern/frontend/src/App.js
+++ sayHiMern/frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
+import EventsPage from './pages/EventsPage';
+import BookingsPage from './pages/BookingsPage';
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Switch>
         <Redirect from="/" to="/auth" exact />
         <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
       </Switch>
     </BrowserRouter>
   );
--- sayHiMern/frontend/src/pages/BookingsPage.js
+++ sayHiMern/frontend/src/pages/BookingsPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class AuthPage extends Component {
+export class BookingsPage extends Component {
     render() {
         return (
             <div>
@@ -10,4 +10,4 @@ export class AuthPage extends Component {
     }
 }
 
-export default AuthPage;
+export default BookingsPage;
