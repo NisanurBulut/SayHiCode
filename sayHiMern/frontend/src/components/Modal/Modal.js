511962ea89674e161bfc9c1f76070a6e91ed39f7
NisanurBulut <nbulut@sarplojistik.com>
2021-03-04 16:09:36 +0300

    styled modal

--- sayHiMern/frontend/src/components/Modal/Modal.js
+++ sayHiMern/frontend/src/components/Modal/Modal.js
@@ -6,12 +6,12 @@ const Modal=(props)=>{
     return (
         <div className={classes.modal}>
             <header className={classes.modalHeader}><h1>{props.title}</h1></header>
-            <section classname={classes.modalContent}>
+            <section className={classes.modalContent}>
                 {props.children}
             </section>
-            <section classname={classes.modalActions}>
-                {props.canCancel && <Button className="btn">Cancel</Button>}
-                {props.canConfirm && <Button className="btn">Confirm</Button>}
+            <section className={classes.modalActions}>
+                {props.canCancel && <Button variant="outlined" color="secondary" className="btn">Cancel</Button>}
+                {props.canConfirm && <Button variant="outlined" color="primary" className="btn">Confirm</Button>}
             </section>
 
         </div>
