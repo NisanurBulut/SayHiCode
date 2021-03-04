a62d1f44e2a2ec183292b655ea5462040abff215
NisanurBulut <nbulut@sarplojistik.com>
2021-03-04 21:54:18 +0300

    styled list items

--- sayHiMern/frontend/src/components/Events/EventItem/EventItem.js
+++ sayHiMern/frontend/src/components/Events/EventItem/EventItem.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import * as classes from './EventItem.module.css';
-import {
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-} from '@material-ui/core';
+import { ListItem, ListItemText, ListItemIcon, ListItemSecondaryAction, Button } from '@material-ui/core';
 import EmojiEventsOutlinedIcon from '@material-ui/icons/EmojiEventsOutlined';
 const EventItem = (props) => {
-  return (
-    <ListItem  className={classes.eventListItem} button key={props.eventId}>
-    <ListItemIcon>
-    <EmojiEventsOutlinedIcon />
-  </ListItemIcon>
-  <ListItemText primary={props.title} secondary={props.description} />
-</ListItem>
+    {console.log(props);}
+    return (
+    <ListItem className={classes.eventListItem} button key={props.eventId}>
+      <ListItemIcon>
+        <EmojiEventsOutlinedIcon />
+      </ListItemIcon>
+      <ListItemText primary={props.eventTitlePrice} secondary={props.description} />
+      <ListItemSecondaryAction>
+         <Button className="btn" color="primary">
+
+         </Button>
+      </ListItemSecondaryAction>
+    </ListItem>
   );
 };
 
--- sayHiMern/frontend/src/components/Events/EventList/EventList.js
+++ sayHiMern/frontend/src/components/Events/EventList/EventList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import * as classes from './EventList.module.css';
-import {List
-} from '@material-ui/core';
+import { List } from '@material-ui/core';
 import EventItem from '../EventItem/EventItem';
 const EventList = (props) => {
-    const eventList=props.events.map((event) => {
-        return (
-         <EventItem key={event._id} eventId={event._id} title={event.title} description={event.description} />
-        );
-      });
+  const eventList = props.events.map((event) => {
+      const eventTitlePrice= `${event.title} ${event.price.toString()} ₺`;
     return (
-        <List className={classes.eventList} component="nav">
-        {eventList}
-      </List>
-    )
-}
+      <EventItem
+        key={event._id}
+        eventId={event._id}
+        eventTitlePrice={eventTitlePrice}
+        description={event.description}
+      />
+    );
+  });
+  return (
+    <List className={classes.eventList} component="nav">
+      {eventList}
+    </List>
+  );
+};
 
-export default EventList
+export default EventList;
