095da91baee11945fb0c5cc5c6f822753072c628
NisanurBulut <nbulut@sarplojistik.com>
2021-03-05 13:21:09 +0300

    listed bookings

--- sayHiMern/frontend/src/pages/BookingsPage/BookingsPage.js
+++ sayHiMern/frontend/src/pages/BookingsPage/BookingsPage.js
@@ -9,22 +9,22 @@ export class BookingsPage extends Component {
     componentDidMount(){
         this.fetchBookings();
     }
-    fetchBookings() {
+    fetchBookings = () => {
         this.setState({ isLoading: true });
         const requestBody = {
           query: `
               query {
                 bookings {
                   _id
-                 createdat
+                 createdAt
                  event {
-                     _id
-                     title
-                     date
+                   _id
+                   title
+                   date
                  }
                 }
               }
-            `,
+            `
         };
 
         fetch('http://localhost:8000/graphql', {
@@ -33,28 +33,30 @@ export class BookingsPage extends Component {
           headers: {
             'Content-Type': 'application/json',
             Authorization: 'Bearer ' + this.context.token
-          },
+          }
         })
-          .then((res) => {
+          .then(res => {
             if (res.status !== 200 && res.status !== 201) {
               throw new Error('Failed!');
             }
             return res.json();
           })
-          .then((resData) => {
-            const bookings = resData.data.bookings;
+          .then(resData => {
+              const bookings = resData.data.bookings;
+              console.log(bookings);
             this.setState({ bookings: bookings, isLoading: false });
           })
-          .catch((err) => {
+          .catch(err => {
+            console.log(err);
             this.setState({ isLoading: false });
           });
-      }
+      };
     render() {
         return (
             <div>
                <ul>
                    {this.state.bookings.map(booking=>{
-                       <li>{booking.date}</li>
+                      return <li key={booking._id}>{booking.createdAt}</li>
                    })}
                </ul>
             </div>
