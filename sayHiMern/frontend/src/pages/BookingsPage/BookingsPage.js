04dc2f0f5e2ec838f650e7acebca063de34ebb8a
NisanurBulut <nbulut@sarplojistik.com>
2021-03-05 13:26:42 +0300

    implemented spinner into bookingPage

--- sayHiMern/frontend/src/pages/BookingsPage/BookingsPage.js
+++ sayHiMern/frontend/src/pages/BookingsPage/BookingsPage.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import AuthContext from '../../context/auth-context';
+import {
+  CircularProgress,
+} from '@material-ui/core';
+import * as classes from './BookingsPage.module.css';
+
 export class BookingsPage extends Component {
-    static contextType = AuthContext;
-    state={
-        isLoading:false,
-        bookings:[],
-    }
-    componentDidMount(){
-        this.fetchBookings();
-    }
-    fetchBookings = () => {
-        this.setState({ isLoading: true });
-        const requestBody = {
-          query: `
+  static contextType = AuthContext;
+  state = {
+    isLoading: false,
+    bookings: [],
+  };
+  componentDidMount() {
+    this.fetchBookings();
+  }
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
               query {
                 bookings {
                   _id
@@ -24,44 +29,48 @@ export class BookingsPage extends Component {
                  }
                 }
               }
-            `
-        };
+            `,
+    };
 
-        fetch('http://localhost:8000/graphql', {
-          method: 'POST',
-          body: JSON.stringify(requestBody),
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + this.context.token
-          }
-        })
-          .then(res => {
-            if (res.status !== 200 && res.status !== 201) {
-              throw new Error('Failed!');
-            }
-            return res.json();
-          })
-          .then(resData => {
-              const bookings = resData.data.bookings;
-              console.log(bookings);
-            this.setState({ bookings: bookings, isLoading: false });
-          })
-          .catch(err => {
-            console.log(err);
-            this.setState({ isLoading: false });
-          });
-      };
-    render() {
-        return (
-            <div>
-               <ul>
-                   {this.state.bookings.map(booking=>{
-                      return <li key={booking._id}>{booking.createdAt}</li>
-                   })}
-               </ul>
-            </div>
-        )
-    }
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        const bookings = resData.data.bookings;
+        console.log(bookings);
+        this.setState({ bookings: bookings, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <CircularProgress className={classes.Spinner} color="secondary" />
+        ) : (
+          <ul>
+            {this.state.bookings.map((booking) => {
+              return <li key={booking._id}>{booking.createdAt}</li>;
+            })}
+          </ul>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default BookingsPage;
