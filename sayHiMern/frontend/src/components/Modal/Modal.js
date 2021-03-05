2b135a22d29fc514a02fa66d04d795a583b10372
NisanurBulut <nbulut@sarplojistik.com>
2021-03-05 21:23:03 +0300

    restyled modal

--- sayHiMern/frontend/src/components/Modal/Modal.js
+++ sayHiMern/frontend/src/components/Modal/Modal.js
@@ -5,7 +5,7 @@ import classes from './Modal.module.css';
 const Modal=(props)=>{
     return (
         <div className={classes.modal}>
-            <header className={classes.modalHeader}><h1>{props.title}</h1></header>
+            <header className={classes.modalHeader}><h1><i>{props.title}</i></h1></header>
             <section className={classes.modalContent}>
                 {props.children}
             </section>
