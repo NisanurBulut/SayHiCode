0c5bf084ff425ee3b3337b1f97d5139298736136
NisanurBulut <nbulut@sarplojistik.com>
2021-03-05 18:28:29 +0300

    listed users's bookings

--- sayHiMern/graphql/resolvers/booking.js
+++ sayHiMern/graphql/resolvers/booking.js
@@ -8,7 +8,7 @@ module.exports = {
       throw new Error('UnAuthenticated!');
     }
     try {
-      const bookings = await Booking.find();
+      const bookings = await Booking.find({ user: req.userId });
       return bookings.map((booking) => {
         return transformBooking(booking);
       });
