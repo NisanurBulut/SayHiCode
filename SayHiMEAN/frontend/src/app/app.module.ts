e00f5b7972ba20e9db130331b3e8c543b005c4f9
NisanurBulut <nbulut@sarplojistik.com>
2021-03-06 20:17:59 +0300

    installed angular apollo

--- SayHiMEAN/frontend/src/app/app.module.ts
+++ SayHiMEAN/frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
