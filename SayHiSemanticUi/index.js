c5a61b7339105c9871fd68ff0324572eae565be6
NisanurBulut <nbulut@sarplojistik.com>
2021-03-09 15:43:06 +0300

    refreshed gitignore

--- SayHiSemanticUi/index.js
+++ SayHiSemanticUi/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -19,7 +21,20 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({port:8000})
-.then(res=>{
-    console.log(`Server running at ${res.url}`);
-})
+mongoose
+  .connect(
+    `mongodb://127.0.0.1:27017/${config.MONGO_DB}?retryWrites=true&w=majority`,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    server.listen({ port: 8000 }).then((res) => {
+      console.log(`Server running at ${res.url}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
