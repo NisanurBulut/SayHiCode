842d17795c8066d5ceceba28eec1207afcde6cd0
NisanurBulut <nbulut@sarplojistik.com>
2021-03-09 22:31:54 +0300

    defined solid for validate user register

--- SayHiSemanticUi/graphql/resolvers/users.js
+++ SayHiSemanticUi/graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
+const { validateRegisterInput } = require('../../util/validators');
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
@@ -13,7 +14,16 @@ module.exports = {
       context,
       info
     ) {
-      // TODO: Validate user data
+      // Validate user data
+      const { errors, valid } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
       // TODO: Make sure user doesnt already exist
       const user = await User.findOne({ username });
       if (user) {
