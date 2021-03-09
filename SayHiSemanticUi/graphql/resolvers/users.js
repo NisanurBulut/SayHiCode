2bee623c469e1abb7352d2e7ff6ff3edad1d7c45
NisanurBulut <nbulut@sarplojistik.com>
2021-03-09 22:13:27 +0300

    implemented register method for users

--- SayHiSemanticUi/graphql/resolvers/users.js
+++ SayHiSemanticUi/graphql/resolvers/users.js
@@ -1,11 +1,38 @@
-const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const  {SECRET_KEY} = require('../../config');
+const User = require('../../models/User');
 module.exports = {
-    Mutation: {
-        register(parent, args, context, info){
-             // TODO: Validate user data
-             // TODO: Make sure user doesnt already exist
-             // TODO: hash password and create an auth token
-        }
-    }
-}
+  Mutation: {
+    async register(
+      parent,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      // TODO: Validate user data
+      // TODO: Make sure user doesnt already exist
+      // TODO: hash password and create an auth token
+
+      password = await bcrypt.hash(password,12);
+      const newUser = User({
+          email,
+          username,
+          password,
+          createdAt: new Date().toISOString()
+      })
+      const res = await newUser.save();
+      const token = jwt.sign({
+          id:res.id,
+          email:res.email,
+          username:res.username
+      },SECRET_KEY, {expiresIn:'1h'});
+      return {
+          ...res._doc,
+          id:res._id,
+          token
+      }
+    },
+  },
+};
