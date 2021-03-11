52be5181e928bd1ad002437cf91761625d880cd7
NisanurBulut <nbulut@sarplojistik.com>
2021-03-11 16:38:40 +0300

    implemented get post book method

--- SayHiSemanticUi/graphql/resolvers/bookPost.js
+++ SayHiSemanticUi/graphql/resolvers/bookPost.js
@@ -1,14 +1,26 @@
 const PostBook = require('../../models/PostBook');
 
 module.exports = {
-    Query: {
-        async getBookPosts() {
-          try {
-            const bookPosts = await PostBook.find();
-            return bookPosts;
-          } catch (err) {
-            throw new Error(err);
-          }
-        },
-      },
-}
+  Query: {
+    async getBookPosts() {
+      try {
+        const bookPosts = await PostBook.find();
+        return bookPosts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getBookPost(_, { postId }) {
+      try {
+        const postBook = await PostBook.findById(postId);
+        if (postBook) {
+          return postBook;
+        } else {
+          throw new Error('Post Book not found !');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
