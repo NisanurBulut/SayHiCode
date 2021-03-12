32ba89cd047e2ea297c54dcafd23d58588399255
NisanurBulut <nbulut@sarplojistik.com>
2021-03-12 22:35:19 +0300

    implemented buttons and inserted link

--- SayHiSemanticUi/client/src/components/PostCard.js
+++ SayHiSemanticUi/client/src/components/PostCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Card, Icon, Label, Image,Button } from 'semantic-ui-react';
 import moment from 'moment';
-
+import { Link } from 'react-router-dom';
 function PostCard({id, author, name, username, likeCount, commentcount,likes,createdAt}) {
-
+  {
+    console.log({id, author, name, username, likeCount, commentcount,likes,createdAt});
+  }
     return (
-        <Card>
+        <Card fluid>
         <Card.Content>
           <Image
             floated='right'
@@ -13,20 +15,30 @@ function PostCard({id, author, name, username, likeCount, commentcount,likes,cre
             src='https://react.semantic-ui.com/images/avatar/large/molly.png'
           />
           <Card.Header>{username}</Card.Header>
-          <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
-          <Card.Description>
+          <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+          <Card.Description as={Link} to={`/bookPosts/${id}`}>
             {name} {author}
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <div className='ui two buttons'>
-            <Button basic color='green'>
-              Approve
-            </Button>
-            <Button basic color='red'>
-              Decline
-            </Button>
-          </div>
+        <Button as='div' labelPosition='right'>
+      <Button color='red'>
+        <Icon name='heart' />
+
+      </Button>
+      <Label as='a' basic color='red' pointing='left'>
+        {likeCount}
+      </Label>
+    </Button>
+    <Button as='div' labelPosition='right'>
+      <Button basic color='blue'>
+        <Icon name='comment' />
+
+      </Button>
+      <Label as='a' basic color='blue' pointing='left'>
+        {commentcount}
+      </Label>
+    </Button>
         </Card.Content>
       </Card>
     )
