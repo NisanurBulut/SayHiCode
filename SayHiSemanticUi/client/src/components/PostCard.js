42d1b0ec4ef8969953fa781d0c41b9e853c6a29a
NisanurBulut <nbulut@sarplojistik.com>
2021-03-12 22:43:30 +0300

    showed data on post card

--- SayHiSemanticUi/client/src/components/PostCard.js
+++ SayHiSemanticUi/client/src/components/PostCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Card, Icon, Label, Image,Button } from 'semantic-ui-react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
-function PostCard({id, author, name, username, likeCount, commentcount,likes,createdAt}) {
-  {
-    console.log({id, author, name, username, likeCount, commentcount,likes,createdAt});
-  }
+function PostCard({
+  bookPost: { name,author, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+
     return (
         <Card fluid>
         <Card.Content>
@@ -17,14 +17,13 @@ function PostCard({id, author, name, username, likeCount, commentcount,likes,cre
           <Card.Header>{username}</Card.Header>
           <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
           <Card.Description as={Link} to={`/bookPosts/${id}`}>
-            {name} {author}
+            {name}-{author}
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
         <Button as='div' labelPosition='right'>
       <Button color='red'>
         <Icon name='heart' />
-
       </Button>
       <Label as='a' basic color='red' pointing='left'>
         {likeCount}
@@ -33,10 +32,9 @@ function PostCard({id, author, name, username, likeCount, commentcount,likes,cre
     <Button as='div' labelPosition='right'>
       <Button basic color='blue'>
         <Icon name='comment' />
-
       </Button>
       <Label as='a' basic color='blue' pointing='left'>
-        {commentcount}
+        {commentCount}
       </Label>
     </Button>
         </Card.Content>
