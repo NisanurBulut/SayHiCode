0f876b727e036bbed59d66aa843d55118294c13b
NisanurBulut <nbulut@sarplojistik.com>
2021-03-12 22:45:38 +0300

    implemented like and comment methods

--- SayHiSemanticUi/client/src/components/PostCard.js
+++ SayHiSemanticUi/client/src/components/PostCard.js
@@ -1,45 +1,55 @@
-import React from 'react'
-import { Card, Icon, Label, Image,Button } from 'semantic-ui-react';
+import React from 'react';
+import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 function PostCard({
-  bookPost: { name,author, createdAt, id, username, likeCount, commentCount, likes }
+  bookPost: {
+    name,
+    author,
+    createdAt,
+    id,
+    username,
+    likeCount,
+    commentCount,
+    likes,
+  },
 }) {
-
-    return (
-        <Card fluid>
-        <Card.Content>
-          <Image
-            floated='right'
-            size='mini'
-            src='https://react.semantic-ui.com/images/avatar/large/molly.png'
-          />
-          <Card.Header>{username}</Card.Header>
-          <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
-          <Card.Description as={Link} to={`/bookPosts/${id}`}>
-            {name}-{author}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-        <Button as='div' labelPosition='right'>
-      <Button color='red'>
-        <Icon name='heart' />
-      </Button>
-      <Label as='a' basic color='red' pointing='left'>
-        {likeCount}
-      </Label>
-    </Button>
-    <Button as='div' labelPosition='right'>
-      <Button basic color='blue'>
-        <Icon name='comment' />
-      </Button>
-      <Label as='a' basic color='blue' pointing='left'>
-        {commentCount}
-      </Label>
-    </Button>
-        </Card.Content>
-      </Card>
-    )
+  const likePost = () => {};
+  const commentOnPost = () => {};
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+        <Card.Description as={Link} to={`/bookPosts/${id}`}>
+          {name}-{author}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="red">
+            <Icon name="heart" />
+          </Button>
+          <Label as="a" basic color="red" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button basic color="blue">
+            <Icon name="comments" />
+          </Button>
+          <Label as="a" basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+      </Card.Content>
+    </Card>
+  );
 }
 
 export default PostCard;
