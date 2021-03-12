3da262dacb2a12edd3641e8c3ae16fdc48c9bf71
NisanurBulut <nbulut@sarplojistik.com>
2021-03-12 21:08:56 +0300

    implemented smart routing

--- SayHiSemanticUi/client/src/components/MenuBar.js
+++ SayHiSemanticUi/client/src/components/MenuBar.js
@@ -1,46 +1,49 @@
 import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState('home');
+  const pathName = window.location.pathname;
+
+  const path = pathName === '/' ? 'home' : pathName.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-      <Menu pointing inverted className="ui green" size="massive">
-        <Menu.Item
-          name="home"
-          active={activeItem === 'home'}
-          onClick={handleItemClick}
-          as={Link}
-          to='/'
-        />
+    <Menu pointing inverted className="ui green" size="massive">
+      <Menu.Item
+        name="home"
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
 
-        <Menu.Menu position="right">
+      <Menu.Menu position="right">
         <Menu.Item
           name="login"
           active={activeItem === 'login'}
           onClick={handleItemClick}
           as={Link}
-          to='/login'
+          to="/login"
         />
         <Menu.Item
           name="register"
           active={activeItem === 'register'}
           onClick={handleItemClick}
           as={Link}
-          to='/register'
+          to="/register"
         />
-          <Menu.Item
-            name="logout"
-            active={activeItem === 'logout'}
-            onClick={handleItemClick}
-            as={Link}
-            to='/logout'
-          />
-        </Menu.Menu>
-      </Menu>
+        <Menu.Item
+          name="logout"
+          active={activeItem === 'logout'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/logout"
+        />
+      </Menu.Menu>
+    </Menu>
   );
 }
 
