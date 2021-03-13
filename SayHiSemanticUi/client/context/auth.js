4359005f894910685f6f95310904cf9f603769e1
NisanurBulut <nbulut@sarplojistik.com>
2021-03-13 19:00:40 +0300

    implemented AuthContext

---
+++ SayHiSemanticUi/client/context/auth.js
@@ -0,0 +1,49 @@
+import React, { useReducer, createContext } from 'react';
+
+const AuthContext = createContext({
+  user: null,
+  login: (userData) => {},
+  logout: () => {},
+});
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
+
+function AuthProvider(props) {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+  function login(userData) {
+    dispatch({
+      type: 'LOGIN',
+      payload: userData,
+    });
+  }
+
+  function logout(userData) {
+    dispatch({
+      type: 'LOGOUT',
+    });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user: state.user, login, logout }}>
+      {...props}
+    </AuthContext.Provider>
+  );
+}
+
+
+export {AuthContext, AuthProvider};
