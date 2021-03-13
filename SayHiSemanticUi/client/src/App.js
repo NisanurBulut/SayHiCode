888963265c77b9799cc67cd7d89a226280c2dd31
NisanurBulut <nbulut@sarplojistik.com>
2021-03-13 20:09:56 +0300

    implemented custom auth routing

--- SayHiSemanticUi/client/src/App.js
+++ SayHiSemanticUi/client/src/App.js
@@ -5,6 +5,7 @@ import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
+import  AuthRoute  from "./util/AuthRoute";
 import { AuthProvider } from './context/auth';
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
@@ -18,8 +19,8 @@ function App() {
         <Container>
           <MenuBar />
           <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
         </Container>
       </Router>
     </AuthProvider>
