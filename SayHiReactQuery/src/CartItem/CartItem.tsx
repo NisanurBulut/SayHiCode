79d560df10eb9022b5ed61b8c2e36d90409eab02
NisanurBulut <nbulut@sarplojistik.com>
2021-03-24 19:34:42 +0300

    implemented CartItem component

--- SayHiReactQuery/src/CartItem/CartItem.tsx
+++ SayHiReactQuery/src/CartItem/CartItem.tsx
@@ -1,4 +1,5 @@
 import Button from '@material-ui/core/Button';
+import { Wrapper } from '../App.style';
 
 import { CartItemType } from '../Types/CartItemType';
 type Props = {
@@ -6,6 +7,39 @@ type Props = {
   addToCart: (clickedItem: CartItemType) => void;
   removeFromCart: (id: number) => void;
 };
-const CartItem: React.FC<Props> = () => <div>CartItem</div>;
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => {
+  return (
+    <Wrapper>
+      <div>
+        <h3>{item.title}</h3>
+        <div className="information">
+          <p>Price: {item.price}₺ </p>
+          <p>Total: {(item.amount * item.price).toFixed(2)}₺</p>
+        </div>
+        <div className="buttons">
+        <p>{item.amount}</p>
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => removeFromCart(item.id)}
+          >
+            -
+          </Button>
+          <p>{item.amount}</p>
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => addToCart(item)}
+          >
+            +
+          </Button>
+        </div>
+      </div>
+      <img src={item.image} alt={item.title} />
+    </Wrapper>
+  );
+};
 
 export default CartItem;
