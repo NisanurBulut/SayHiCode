1ce04b92b047bd326a105d4450a2c084cd443fcb
NisanurBulut <nbulut@sarplojistik.com>
2021-03-25 18:28:45 +0300

    refreshed packages

--- SayHiPHP/resources/views/layouts/app.blade.php
+++ SayHiPHP/resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                   Say Hi PHP
+                   <img src="/images/logo.jpg" alt="" style="max-height:25px; max-width:"/>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -32,7 +33,10 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav">
+                    <li class="nav-item">Say Hi PHP
+                    <img src="/images/logo.jpg" alt="" style="max-height:25px; max-width:25px;"/>
+                    </li>
 
                     </ul>
 
@@ -45,7 +49,7 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
