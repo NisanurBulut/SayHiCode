c0ff857d2c04717f1574c6be8d152eaa40c0d689
NisanurBulut <nbulut@sarplojistik.com>
2021-03-25 21:36:04 +0300

    restyled app

--- SayHiLaravel/resources/views/layouts/app.blade.php
+++ SayHiLaravel/resources/views/layouts/app.blade.php
@@ -5,8 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Say Hi Laravel</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+<li>
+    <a href=""><a/>
+</li>
+<li><a href=""></a></li>
+    </nav>
     @yield('content')
 </body>
 </html>
