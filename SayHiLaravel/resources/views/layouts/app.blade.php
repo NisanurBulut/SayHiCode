e0ef265931b3e7931541fd8ae1653847024bfb1a
NisanurBulut <nbulut@sarplojistik.com>
2021-03-25 22:06:17 +0300

    implemented navbar and basic home components

--- SayHiLaravel/resources/views/layouts/app.blade.php
+++ SayHiLaravel/resources/views/layouts/app.blade.php
@@ -5,15 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Say Hi Laravel</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
+    <meta charset="UTF-8" />
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white flex justify-between">
+<body class="bg-red-200">
+<nav class="p-6 text-white flex items-center justify-between flex-wrap bg-red-500 p-6 mb-6">
+  <div class="flex items-center flex-no-shrink mr-6">
+    <svg class="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">Say Hi Laravel!!</span>
+  </div>
+<ul class="flex items-center">
 <li>
-    <a href=""><a/>
+<a class="p-6" href="">Home</a>
 </li>
-<li><a href=""></a></li>
-    </nav>
+<li>
+<a class="p-6" href="">Dashboard</a>
+</li>
+<li>
+<a class="p-6" href="">Post</a>
+</li>
+</ul>
+<ul class="flex items-center">
+<li>
+<a class="p-6" href="">Nisanur BULUT</a>
+</li>
+<li>
+<a class="p-6" href="">login</a>
+</li>
+<li>
+<a class="p-6" href="">Logout</a>
+</li>
+</ul>
+</nav>
+</div>
     @yield('content')
 </body>
 </html>
--- SayHiLaravel/resources/views/posts/index.blade.php
+++ SayHiLaravel/resources/views/posts/index.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
 @section('content')
-Merhaba Nisanur !
+<div class="flex justify-center">
+<div class="w-8/12 bg-white p-6 rounded-lg">
+Post</div>
+</div>
 @endsection
