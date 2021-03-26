18e3d672132be676f182c70eaf4c93cae30cb802
NisanurBulut <nbulut@sarplojistik.com>
2021-03-26 20:35:34 +0300

    restyled navbar

--- SayHiLaravel/resources/views/layouts/app.blade.php
+++ SayHiLaravel/resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
 </head>
 
 <body class="bg-gray-100">
-    <nav class="p-6 text-white flex items-center justify-between flex-wrap bg-red-700 p-6 mb-6">
-        <div class="flex text-gray-100 items-center flex-no-shrink mr-6">
-            <svg class="text-gray-100 h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
+    <nav class="p-6 text-white flex items-center justify-between flex-wrap bg-purple-900 p-6 mb-6">
+        <div class="flex items-center flex-no-shrink mr-4">
+            <svg class="fill-current text-white h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
             xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
