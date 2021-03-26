afae5c49e9ae7ec5ca2a1fde4a8016f29faee6d0
NisanurBulut <nbulut@sarplojistik.com>
2021-03-26 22:33:16 +0300

    after unlike again liked doesnt send email

--- SayHiLaravel/app/Http/Controllers/PostLikeController.php
+++ SayHiLaravel/app/Http/Controllers/PostLikeController.php
@@ -20,8 +20,13 @@ class PostLikeController extends Controller
         $post->likes()->create([
             'user_id'=>$request->user()->id
         ]);
+
         $user = auth()->user();
-        Mail::to($post->user)->send(new PostLiked(auth()->user(),$post));
+
+        if(!$post->likes()->onlyTrashed()->where('user_id', $request->user()->id)->count()){
+
+            Mail::to($post->user)->send(new PostLiked(auth()->user(),$post));
+        }
       return back();
     }
     public function destroy(Post $post, Request $request)
