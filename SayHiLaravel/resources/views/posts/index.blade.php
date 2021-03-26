08f0db87457f81e893c56382ef242ab0cf24ad69
NisanurBulut <nbulut@sarplojistik.com>
2021-03-26 16:18:41 +0300

    restyled

--- SayHiLaravel/resources/views/posts/index.blade.php
+++ SayHiLaravel/resources/views/posts/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
+    <div class="focus:outline-none flex justify-center">
+        <div class="focus:outline-none w-8/12 bg-white p-6 rounded-lg">
             <form action="{{ route('posts') }}" method="POST">
                 @csrf
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4"
-                    class="bg-gray-100 border-2 w-full -p-4 rounded-lg
+                    class="focus:outline-none bg-gray-100 border-2 w-full -p-4 rounded-lg
                                                 @error('body')
                                                 border-red-500
                                                 @enderror
@@ -20,7 +20,7 @@
                 </div>
                 <div>
                     <button type="submit"
-                    class="bg-green-600 px-4 py-3 text-white rounded font-medium w-full">Save</button>
+                    class="focus:outline-none bg-green-600 px-4 py-3 text-white rounded font-medium w-full">Save</button>
                 </div>
             </form>
             @if ($posts->count())
@@ -50,14 +50,14 @@
                                             <form action={{ route('posts.likes', $post) }} method="POST" class="mr-1">
                                                 @csrf
                                                 <button id="likeBtn" type="submit"
-                                                    class="mr-3 inline-flex items-center cursor-pointer">
+                                                    class="focus:outline-none mr-3 inline-flex items-center cursor-pointer">
                                                     <svg class="fill-heart text-red-700 inline-block h-7 w-7 heart"
                                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                         stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                             d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                                     </svg>
-                                                    <span class="text-gray-600 text-sm font-bold">
+                                                    <span class="focus:outline-none text-gray-600 text-sm font-bold">
                                                         {{ $post->likes->count() }}
                                                         {{ Str::plural('Like', $post->likes->count()) }}</span>
                                                 </button>
@@ -82,12 +82,10 @@
                                         @endif
                                     @endauth
 
-
-
                                     <form action={{ route('posts.destroy', $post) }} method="post">
                                         @csrf
                                         @method('DELETE')
-                                        <button id="deleteBtn" class="mr-3 inline-flex items-center cursor-pointer">
+                                        <button id="deleteBtn" class="focus:outline-none mr-3 inline-flex items-center cursor-pointer">
                                             <svg class="fill-trash text-red-700 inline-block h-7 w-7 trash"
                                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor">
@@ -95,10 +93,6 @@
                                             </svg>
                                         </button>
                                     </form>
-
-
-
-
                                 </div>
 
                             </div>
