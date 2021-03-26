59f11be9982768d55ee5f7c117762d8a80da978a
NisanurBulut <nbulut@sarplojistik.com>
2021-03-26 23:14:24 +0300

    deleted unused imports

--- SayHiLaravel/routes/web.php
+++ SayHiLaravel/routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\UserPostController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
