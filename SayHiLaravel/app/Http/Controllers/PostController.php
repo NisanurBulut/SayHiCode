9819fe25bf75e9f4dae1e6e1592edaa7f096bea7
NisanurBulut <nbulut@sarplojistik.com>
2021-03-26 16:43:11 +0300

    posts listed order desc

--- SayHiLaravel/app/Http/Controllers/PostController.php
+++ SayHiLaravel/app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with(['user','likes'])->paginate(5); // Collection
+        $posts = Post::orderBy('created_at','desc')->with(['user','likes'])->paginate(5); // Collection
 
         return view('posts.index',[
         'posts'=>$posts
@@ -25,7 +25,6 @@ class PostController extends Controller
 
         return back();
     }
-
     public function destroy(Post $post)
     {
         $post->delete();
