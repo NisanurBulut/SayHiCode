62935725d1e168a14dff41bc553f7323da588766
NisanurBulut <nbulut@sarplojistik.com>
2021-03-26 13:15:58 +0300

    formatted like text

--- SayHiLaravel/resources/views/posts/index.blade.php
+++ SayHiLaravel/resources/views/posts/index.blade.php
@@ -7,10 +7,10 @@
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full -p-4 rounded-lg
-                                    @error('body')
-                                                    border-red-500
-                                    @enderror
-                                    " placeholder="Say Something!"></textarea>
+                                        @error('body')
+                                                            border-red-500
+                                        @enderror
+                                        " placeholder="Say Something!"></textarea>
                     @error('body')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
@@ -28,7 +28,7 @@
                         <div class="m-3">
                             <h2 class="text-lg mb-2">{{ $post->user->username }}
                                 <span class="text-sm text-teal-800 font-mono bg-teal-100
-                                        inline rounded-full px-2 align-top float-right animate-pulse">
+                                            inline rounded-full px-2 align-top float-right animate-pulse">
                                     {{ $post->created_at->diffForHumans() }}</span>
                             </h2>
                             <p
@@ -41,17 +41,24 @@
                                 <div class="mb-2">
                                     <div class="flex items-center">
                                         <span class="mr-3 inline-flex items-center cursor-pointer">
-                                            <svg class="fill-heart text-red-700 inline-block h-7 w-7 heart" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                            <svg class="fill-heart text-red-700 inline-block h-7 w-7 heart"
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                             </svg>
                                         </span>
                                         <span class="mr-3 inline-flex items-center cursor-pointer">
-                                            <svg class="fill-heart text-red-700 inline-block h-7 w-7 heart" xmlns="http://www.w3.org/2000/svg" fill="full" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                            <svg class="fill-heart text-red-700 inline-block h-7 w-7 heart"
+                                                xmlns="http://www.w3.org/2000/svg" fill="full" viewBox="0 0 24 24"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                             </svg>
                                         </span>
                                     </div>
-                                    <span class="text-gray-600 text-sm font-bold">{{ $post->likes->count() }} Likes</span>
+                                    <span class="text-gray-600 text-sm font-bold">
+                                        {{ $post->likes->count() }} {{ Str::plural('Like',$posts->likes->count()) }}</span>
                                 </div>
                             </div>
                         </div>
