e867a867b9f58e41e6530c86dd0f352c4d2d664a
NisanurBulut <nbulut@sarplojistik.com>
2021-03-27 11:41:09 +0300

    implemented welcome page with styles

--- SayHiPHP/resources/views/welcome.blade.php
+++ SayHiPHP/resources/views/welcome.blade.php
@@ -18,19 +18,13 @@
     @endif
 
     <div class="content">
+        <img src="/img/pizza-house.png" alt="Pizza House" />
         <div class="title m-b-md">
-            Laravel
+            The Eskişehir's Best Pizzas
         </div>
 
         <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+
         </div>
     </div>
 </div>
