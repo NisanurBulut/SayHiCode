9cf1882ad4873eb66d5df00b06d84ad1b07d6e33
NisanurBulut <nbulut@sarplojistik.com>
2021-03-27 21:54:21 +0300

    implemendt guard routing

--- SayHiPHP/routes/web.php
+++ SayHiPHP/routes/web.php
@@ -8,11 +8,11 @@ use App\Http\Controllers\PizzaController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/pizzas', [PizzaController::class, 'index'])->name('pizzas');
-Route::get('/pizzas/{id}',[PizzaController::class, 'show']);
+Route::get('/pizzas', [PizzaController::class, 'index'])->name('pizzas')->middleware('auth');
+Route::get('/pizzas/{id}',[PizzaController::class, 'show'])->middleware('auth');
 Route::get('/create', [PizzaController::class, 'create'])->name('create');
 Route::post('/pizzas', [PizzaController::class, 'store'])->name('store');
-Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy'])->name('destroy');
+Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy'])->name('destroy')->middleware('auth');
 
 Auth::routes();
 
