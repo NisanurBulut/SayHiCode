0eb6b9d4dca779f89296cd9b606a10fcebd9ed5e
NisanurBulut <nbulut@sarplojistik.com>
2021-04-01 22:07:19 +0300

    inserted new routes

--- SayHiPhpMvc/core/Router.php
+++ SayHiPhpMvc/core/Router.php
@@ -37,6 +37,7 @@ class Router
         }
         if (is_array($callback)) {
             Application::$app->controller = new $callback[0]();
+            $callback[0] = Application::$app->controller;
         }
         return call_user_func($callback, $this->request);
     }
