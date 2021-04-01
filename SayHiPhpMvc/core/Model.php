543931b6d9048a142bc7921fd6d74228e4dea4cb
NisanurBulut <nbulut@sarplojistik.com>
2021-04-01 23:32:28 +0300

    created model base validation

--- SayHiPhpMvc/core/Model.php
+++ SayHiPhpMvc/core/Model.php
@@ -37,23 +37,25 @@ abstract class Model
                     $this->addError($attribute, self::RULE_EMAIL);
                 }
                 if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
-                    $this->addError($attribute, self::RULE_MIN);
+                    $this->addError($attribute, self::RULE_MIN,$rule);
                 }
                 if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
-                    $this->addError($attribute, self::RULE_MAX);
+                    $this->addError($attribute, self::RULE_MAX,$rule);
                 }
             }
         }
         return empty($this->errors);
     }
-    public function addError(string $attribute, string $rule)
+    public function addError(string $attribute, string $rule, $params = [])
     {
         $message = $this->errorMessages()[$rule] ?? '';
+        foreach ($params as $key => $value) {
+            $message = str_replace("{{$key}}", $value, $message);
+        }
         $this->errors[$attribute][] = $message;
     }
     public function errorMessages()
     {
-
         return [
             self::RULE_REQUIRED => 'This field is required',
             self::RULE_EMAIL => 'This field must be valid email address',
