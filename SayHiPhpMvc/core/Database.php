820da07623d1d9ee46711279db1af646f5f511f3
NisanurBulut <nbulut@sarplojistik.com>
2021-04-02 13:43:45 +0300

    implemented migration.php

--- SayHiPhpMvc/core/Database.php
+++ SayHiPhpMvc/core/Database.php
@@ -1,13 +1,42 @@
 <?php
+
 namespace app\core;
-class Database {
+
+class Database
+{
     public \PDO $pdo;
 
-public function __construct(array $config){
-    $dsn = $config['dsn'] ?? '';
-    $user = $config['user'] ?? '';
-    $password = $config['password'] ?? '';
-    $this->pdo = new \PDO($dsn, $user,$password);
-    $this->pdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
-}
+    public function __construct(array $config)
+    {
+        $dsn = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
+        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function applyMigration()
+    {
+        $this->createMigrationsTable();
+        $this->getAppliedMigrations();
+
+        $files=scandir(Application::$ROOT_DIR.'/migrations');
+
+
+    }
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;");
+    }
+    public function getAppliedMigrations()
+    {
+        $statement = $this->pdo->prepare("SELECT migration FROM migrations");
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
 }
