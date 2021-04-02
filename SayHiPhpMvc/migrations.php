5d6f25df83f445f9425d39d25517dfb7f749cc71
NisanurBulut <nbulut@sarplojistik.com>
2021-04-02 14:05:35 +0300

    formatted document

--- SayHiPhpMvc/migrations.php
+++ SayHiPhpMvc/migrations.php
@@ -3,17 +3,17 @@
 
 use app\core\Application;
 
-require_once  __DIR__.'/vendor/autoload.php';
+require_once  __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-$config=[
+$config = [
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USER'],
         'password' => $_ENV['DB_PASSWORD'],
     ]
-    ];
+];
 
 $app = new Application(__DIR__, $config);
 
