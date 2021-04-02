17c14cac1aab41da996de36724b5433b2f51b975
NisanurBulut <nbulut@sarplojistik.com>
2021-04-02 14:55:29 +0300

    implemented save migration

---
+++ SayHiPhpMvc/migrations/m0002_createusers.php
@@ -0,0 +1,28 @@
+<?php
+
+use app\core\Application;
+
+class m0002_createusers
+{
+
+    public function up()
+    {
+        $db = Application::$app->db;
+        $SQL = "CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            firstname VARCHAR(255) NOT NULL,
+            lastname VARCHAR(255) NOT NULL,
+            status TINYINT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )  ENGINE=INNODB;";
+        $db->pdo->exec($SQL);
+    }
+
+    public function down()
+    {
+        $db = Application::$app->db;
+        $SQL = "DROP TABLE users;";
+        $db->pdo->exec($SQL);
+    }
+}
