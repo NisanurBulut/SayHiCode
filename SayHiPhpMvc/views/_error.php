9e6074d37daf9e22dd5b7a284053a8629429e947
NisanurBulut <nbulut@sarplojistik.com>
2021-04-04 00:17:00 +0300

    implemented template for error page

--- SayHiPhpMvc/views/_error.php
+++ SayHiPhpMvc/views/_error.php
@@ -2,5 +2,15 @@
 
 ?>
 
-<h1><?php echo $exception->getCode() ?>
--<?php echo $exception->getMessage() ?></h1>
+
+<div class="page-wrap d-flex flex-row align-items-center" style=" min-height: 100vh;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block"><?php echo $exception->getCode() ?></span>
+                <div class="mb-4 lead"><?php echo $exception->getMessage() ?></div>
+                <a href="/" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
