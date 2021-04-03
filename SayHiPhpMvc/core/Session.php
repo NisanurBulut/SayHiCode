c4a336a265502499076247d5b639a947e351d0dc
NisanurBulut <nbulut@sarplojistik.com>
2021-04-03 18:44:08 +0300

    implemented get and set methods

--- SayHiPhpMvc/core/Session.php
+++ SayHiPhpMvc/core/Session.php
@@ -27,7 +27,18 @@ class Session
     {
         return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
     }
-
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+    public function get($key)
+    {
+        return $_SESSION[$key] ?? false;
+    }
+    public function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
     public function __destruct()
     {
         $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
