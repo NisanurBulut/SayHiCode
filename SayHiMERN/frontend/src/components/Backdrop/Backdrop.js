122b9e612e5df5677d632b45ad925b7a7b174f4d
NisanurBulut <nbulut@sarplojistik.com>
2021-04-14 22:08:20 +0300

    refreshed project

--- SayHiMERN/frontend/src/components/Backdrop/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import * as classes from './Backdrop.module.css';
-
-const Backdrop = props => <div className={classes.backdrop}></div>;
-
-export default Backdrop;
--- SayHiMERN/frontend/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Backdrop from './Backdrop/Backdrop';
-import Modal from './Modal/Modal';
-import MainNavigation from './Navigation/MainNavigation';
-export { Modal, Backdrop, MainNavigation };
