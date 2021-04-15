bc3b720971c51ce30a780160224454bf332902cb
NisanurBulut <nbulut@sarplojistik.com>
2021-04-15 09:56:53 +0300

    renamed project

---
+++ SayHiMERN/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const grapqlSchema=require('./graphql/schema/index');
+const graphqlResolvers=require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+const app = express();
+
+app.use(isAuth);
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method==='OPTIONS'){
+    return res.sendStatus(200);
+  }
+  next();
+});
+app.use(
+  '/smartBookingApi',
+  graphqlHTTP({
+    schema: grapqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(`mongodb://127.0.0.1:27017/${process.env.MONGO_DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
