a4c5092470eb16b0f248ca9d510bd8d9d5e0b12b
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-26 15:17:57 +0300

    implemented global theme

--- SayHiStyledComponent/src/index.tsx
+++ SayHiStyledComponent/src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { sayHiRepoTheme } from "./sayhirepo-theme";
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={sayHiRepoTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
---
+++ SayHiStyledComponent/src/sayhirepo-theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const sayHiRepoTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+};
+
+export { sayHiRepoTheme };
