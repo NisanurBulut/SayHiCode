50721a3bc7c7098cd532d79f73e29affcecf7b98
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-27 23:06:22 +0300

    implemented search text field

--- SayHiReactTypeScriptTemplate/src/components/CountryList.tsx
+++ SayHiReactTypeScriptTemplate/src/components/CountryList.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 import { CountryType } from "../types";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import { CountryItem, Loading } from "../components";
-
+import SearchTwoTone from "@material-ui/icons/SearchTwoTone";
+import {
+  IconButton,
+  InputBase
+} from "@material-ui/core";
+import { Paper, makeStyles } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  searchPaper: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    marginBottom: theme.spacing(4),
+  },
+  searchInput: {
+    marginLeft: theme.spacing(1.5),
+    flex: 1,
   },
   paper: {
     padding: theme.spacing(2),
@@ -22,16 +36,31 @@ const getCountries = async () => {
   );
   return countries;
 };
+
 function CountryList() {
   const classes = useStyles();
+  const [searchKey, setSearchKey] = useState("");
   const { status, data, error, isLoading } = useQuery(
     "countries",
     getCountries
   );
-  console.log(isLoading);
+  useEffect(() => {
+    
+  }, []);
   return (
     <div className={classes.root}>
-        <Grid container spacing={4}>
+        <Paper className={classes.searchPaper}>
+        <InputBase
+          placeholder="Search countries"
+          className={classes.searchInput}
+          value={searchKey}
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <IconButton>
+          <SearchTwoTone />
+        </IconButton>
+      </Paper>
+      <Grid container spacing={4}>
         <Loading isLoading={isLoading}>
           {data?.data.map((item) => {
             return (
@@ -40,8 +69,8 @@ function CountryList() {
               </Grid>
             );
           })}
-      </Loading>
-        </Grid>
+        </Loading>
+      </Grid>
     </div>
   );
 }
