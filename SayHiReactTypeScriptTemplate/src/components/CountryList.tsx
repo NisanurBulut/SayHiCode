1bf8e4cf5d2644133b694c1d48a1b33e979a00e6
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-27 12:12:11 +0300

    listed countries data via reactquery

--- SayHiReactTypeScriptTemplate/src/components/CountryList.tsx
+++ SayHiReactTypeScriptTemplate/src/components/CountryList.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import axios from 'axios';
-import { useQuery } from 'react-query';
- function CountryList() {
-    const { status, data, error, isFetching } = useQuery('todos', async () => {
-        const { data } = await axios.get('https://restcountries.eu/rest/v2/all')
-        console.log(data);
-        return data;
-      })
-    return (
-        <div>
-            
-        </div>
-    )
+import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { CountryType } from "../types";
+
+
+const getCountries = async () => {
+    const countries = await axios.get<CountryType[]>("https://restcountries.eu/rest/v2/all");
+    return countries;
+  };
+function CountryList() {
+  const { status, data, error, isFetching } = useQuery('countries',getCountries);
+  console.log(status, data, error, isFetching);
+  return <div></div>;
 }
 
 export default CountryList;
