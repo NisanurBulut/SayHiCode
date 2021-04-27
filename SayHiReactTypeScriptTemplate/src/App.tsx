532cdda1c90bc5ea6e3af1090ced2af0b8e4d72b
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-27 14:35:29 +0300

    applied react material ui

--- SayHiReactTypeScriptTemplate/src/App.tsx
+++ SayHiReactTypeScriptTemplate/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import CountryList from './components/CountryList';
+import Container from '@material-ui/core/Container';
 
 function App() {
     const queryClient = new QueryClient();
     return (
         <QueryClientProvider client={queryClient}>
-            <CountryList />
+           <Container maxWidth="md">
+           <CountryList />
+           </Container>
         </QueryClientProvider>
     )
 }
