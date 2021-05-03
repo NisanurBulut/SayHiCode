578d41bd59f7bd534d4a12ad93a19d391bc70ead
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-03 14:36:05 +0300

    implemented enums

---
+++ SayHiXAML/core/EnumLessons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SayHiXAML.core
+{
+    public enum EnumLessons
+    {
+        [Description("Türkçe")]
+        TURKCE=0,
+
+        [Description("Matematik")]
+        MATEMATIK = 1,
+
+        [Description("FEN")]
+        FEN = 2,
+
+        [Description("SOSYAL")]
+        SOSYAL = 3
+    }
+}
---
+++ SayHiXAML/core/IExamStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SayHiXAML.core
+{
+    public interface IExamStrategy
+    {
+    }
+}
