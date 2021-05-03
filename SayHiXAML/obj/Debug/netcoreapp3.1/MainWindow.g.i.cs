7b04be1dbec7bc6c7eaba7fd74d2f70f8387f628
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-03 13:31:29 +0300

    inserted new images

--- SayHiXAML/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ SayHiXAML/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace SayHiXAML {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SayHiXAML;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SayHiXAML;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
