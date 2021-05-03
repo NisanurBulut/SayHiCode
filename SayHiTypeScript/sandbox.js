df72828adc42b57ef1b304b34803f33556961885
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-03 22:38:50 +0300

    created sayhitypescript project

---
+++ SayHiTypeScript/sandbox.js
@@ -0,0 +1,6 @@
+var character = 'nisanur';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ SayHiTypeScript/sandbox.ts
@@ -0,0 +1,9 @@
+const character = 'nisanur';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(input);
+});
