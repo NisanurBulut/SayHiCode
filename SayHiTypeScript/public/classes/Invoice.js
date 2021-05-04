7e01888ca77ab2be030f39d1d0fc8b34a4298fa9
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-04 13:57:13 +0300

    created classes

---
+++ SayHiTypeScript/public/classes/Invoice.js
@@ -0,0 +1,12 @@
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + "\u20BA for " + this.details;
+    };
+    return Invoice;
+}());
+export { Invoice };
