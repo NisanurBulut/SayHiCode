b622e41e9349c9d0951e0197abcf1a7fcfb74441
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-04 13:57:32 +0300

    tsc app.ts

--- SayHiTypeScript/public/app.js
+++ SayHiTypeScript/public/app.js
@@ -1,1 +1,19 @@
-"use strict";
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+var form = document.querySelector('.new-item-form');
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+});
--- SayHiTypeScript/src/app.ts
+++ SayHiTypeScript/src/app.ts
@@ -1,6 +1,24 @@
 import {Invoice} from './classes/Invoice.js';
 import {Payment} from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
 
-const invOne = new Invoice('Nisanur','work on the winx website',250);
-const invTwo = new Invoice('Furkan','work on the winx website',250);
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e:Event)=>{
+    e.preventDefault();
+    let doc : HasFormatter;
+    if(type.value==='invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }else{
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+})
+
