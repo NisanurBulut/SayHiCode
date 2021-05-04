50de4bfea045963b867dac52b199637e340324f0
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-04 15:11:33 +0300

    implemented generics

--- SayHiTypeScript/public/app.js
+++ SayHiTypeScript/public/app.js
@@ -1,23 +1,43 @@
-import { Invoice } from './classes/Invoice.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-import { Payment } from './classes/Payment.js';
-var form = document.querySelector('.new-item-form');
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+var form = document.querySelector(".new-item-form");
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
 // list template instance
-var ul = document.querySelector('ul');
+var ul = document.querySelector("ul");
 var list = new ListTemplate(ul);
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     var doc;
-    if (type.value === 'invoice') {
+    if (type.value === "invoice") {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    list.render(doc, type.value, 'end');
+    list.render(doc, type.value, "end");
 });
+var addUID = function (obj) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, obj), { uid: uid });
+};
+var docThree = {
+    uid: 1,
+    resourceName: "person",
+    data: "Nisanur",
+};
--- SayHiTypeScript/src/app.ts
+++ SayHiTypeScript/src/app.ts
@@ -1,28 +1,44 @@
-import {Invoice} from './classes/Invoice.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-import {Payment} from './classes/Payment.js';
-import { HasFormatter } from './interfaces/HasFormatter.js';
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 // inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
 // list template instance
-const ul = document.querySelector('ul')!;
+const ul = document.querySelector("ul")!;
 const list = new ListTemplate(ul);
 
-form.addEventListener('submit', (e:Event)=>{
-    e.preventDefault();
-    let doc : HasFormatter;
-    if(type.value==='invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-    }else{
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    list.render(doc, type.value,'end');
-})
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  list.render(doc, type.value, "end");
+});
+
+const addUID = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: string;
+}
+
+const docThree: Resource<string> = {
+  uid: 1,
+  resourceName: "person",
+  data: "Nisanur",
+};
