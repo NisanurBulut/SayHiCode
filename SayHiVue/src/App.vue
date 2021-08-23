d7ee5a9fb83a7872d3b47849d26b7e6db9d5728a
Nisanur Bulut <nisanur.bulut@siemens.com>
2021-08-23 21:31:26 +0300

    created Character interface

--- SayHiVue/src/App.vue
+++ SayHiVue/src/App.vue
@@ -1,13 +1,17 @@
-<template>
-</template>
-
-<script>
-
-export default {
+<script langs="ts">
+import { defineComponent, ref} from 'vue';
+import Character from './types/Character';
+export default defineComponent({
   name: 'App',
-  components: {
+  components:{},
+  setup(){
+    const characters=ref<Character[]>([
+      {
+        
+      }
+    ]);
   }
-}
+});
 </script>
 
 <style>
